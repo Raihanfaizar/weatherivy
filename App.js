8e9203b9f20f61fc9d4766be867ca2c7b9a82720
mrkhairullah <mrkhairullah.id@gmail.com>
2023-07-05 15:03:41 +0700

    Update app

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './sources/screens/HomeScreen';
+import SearchScreen from './sources/screens/SearchScreen';
+import ForecastScreen from './sources/screens/ForecastScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="white" />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            elevation: 0
+          },
+          tabBarShowLabel: true,
+          tabBarStyle: {
+            height: 65,
+            paddingBottom: 8
+          },
+          tabBarItemStyle: {
+            paddingTop: 8
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "bold"
+          },
+          tabBarActiveTintColor: "#1d4ed8",
+          tabBarInactiveTintColor: "#94a3b8",
+          tabBarHideOnKeyboard: true
+        }}
+      >
+        <Tab.Screen
+          name="Overview"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="partly-sunny-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Forecast"
+          component={ForecastScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="list" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
